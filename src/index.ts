91e1abc79baf37affd49007428b6b4482d66b0bd
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-07-27 14:07:14 +0900

    Convert issue to post

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
 import core from '@actions/core';
 import { Octokit } from 'octokit';
+import Hexo from 'hexo';
 
-const issueUrl = core.getInput('issue_url');
-const index = issueUrl.indexOf('/repos');
-const endpoint = issueUrl.substring(index);
-const token = core.getInput('token');
+const MILESTONE_PUBLISH = 'publish';
 
-const gh = new Octokit({ auth: token });
+try {
+    const issueUrl = core.getInput('issue_url');
+    const index = issueUrl.indexOf('/repos');
+    const endpoint = issueUrl.substring(index);
+    const token = core.getInput('token');
 
-console.log(endpoint);
-gh.request(`GET ${endpoint}`).then((response) => {
-    const { title, updated_at, labels, milestone, body } = response.data;
-    console.log({ title, updated_at, labels, milestone, body });
-}).catch((reason) => {
-    core.setFailed(reason);
-});
+    const gh = new Octokit({ auth: token });
+
+    const hexo = new Hexo(process.cwd(), {});
+    hexo.init().then(() => {
+        gh.request(`GET ${endpoint}`).then((response) => {
+            const { title, updated_at: date, labels, milestone, body: content } = response.data;
+            if (milestone.title !== MILESTONE_PUBLISH) {
+                console.log(`Issue ${endpoint} does not have milestone ${MILESTONE_PUBLISH}`);
+            } else {
+                const tags = labels.map((label: any) => label.name);
+                hexo.post.create({
+                    title,
+                    date,
+                    tags,
+                    content,
+                } as any);
+            }
+        }).catch((reason) => {
+            core.setFailed(reason);
+        });
+    }).catch((reason) => {
+        core.setFailed(reason);
+    });
+} catch (e) {
+    core.setFailed(e);
+}
